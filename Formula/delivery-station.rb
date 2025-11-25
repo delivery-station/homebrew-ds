1f261ba4ffeec0647f31bebac8e6467a90852876
Alexander Kharkevich <alex@kharkevich.org>
2025-11-25 16:11:34 -0500

    feat: initial release

---
+++ Formula/delivery-station.rb
@@ -0,0 +1,45 @@
+class DeliveryStation < Formula
+  desc "Delivery Station binary executable"
+  homepage "https://github.com/delivery-station/ds"
+  version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/delivery-station/ds/releases/download/v#{version}/ds-darwin-arm64.tar.gz"
+      sha256 "5c6f3fbd74f359f99b2d62fa6ef1d110f61b67a3db00aedbfc0ccbdd4d2a1e25"
+    end
+
+    on_intel do
+      url "https://github.com/delivery-station/ds/releases/download/v#{version}/ds-darwin-amd64.tar.gz"
+      sha256 "75a27da52911cd0bdb7340ce8edc5cecff7a34a6d43e96f0c3829cfb3379b3d7"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/delivery-station/ds/releases/download/v#{version}/ds-linux-arm.tar.gz"
+      sha256 "16a01df8575763f1ed00884af8c58011ac2670e77c68f8f935ea3ff3cd444e00"
+    end
+
+    on_intel do
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/delivery-station/ds/releases/download/v#{version}/ds-linux-amd64.tar.gz"
+        sha256 "6952fa49521646f05377a7a3b42f28b3d5be0bd60763924a09ab2f449ad66286"
+      end
+    end
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install "ds"
+  end
+
+  test do
+    system "#{bin}/ds", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,39 @@
+# Delivery Station Homebrew Tap
+
+This tap provides a Homebrew formula for installing Delivery Station (ds).
+
+## Installation
+
+### Add this tap
+
+```bash
+brew tap delivery-station/ds
+```
+
+### Install Delivery Station
+
+```bash
+brew install delivery-station
+```
+
+## Usage
+
+After installation, run:
+
+```bash
+ds --version
+ds --help
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade delivery-station
+```
+
+## Uninstallation
+
+```bash
+brew uninstall delivery-station
+```
